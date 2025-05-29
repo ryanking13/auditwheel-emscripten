ef4c0860e26dead651fb19a7ecf5f5fb0b814450
Gyeongjae Choi <def6488@gmail.com>
2025-05-29 22:56:53 +0900

    Improve `show` command output to include rpath information (#49)
    * Use python 3.13
    
    * changelog
    
    * Show runtime paths
    
    * Add test
    
    * Update readme

--- auditwheel_emscripten/cli/main.py
+++ auditwheel_emscripten/cli/main.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 import typer
-from rich.pretty import pprint
 
 from .. import function_type, get_exports, get_imports, repair, show
+from ..show import locate_dependency
 from ..wheel_utils import unpack_if_wheel
 
 app = typer.Typer()
@@ -14,18 +14,55 @@ def main():
     """Auditwheel-like tool for emscripten wheels and shared libraries."""
 
 
+def print_dylib(
+    library: str,
+    deps: list[str],
+    runtime_paths: list[str],
+    libraries: list[str],
+    show_runtime_paths: bool = False,
+):
+    """
+    Print shared library dependencies and runtime paths.
+    The output looks similar to the output of `ldd`
+    """
+    print(f"{library}:")
+
+    for dep in deps:
+        line = f"\t{dep:>10}"
+        deppath = locate_dependency(library, dep, libraries, runtime_paths)
+        if deppath:
+            line += f" => {deppath}"
+        print(line)
+
+    if show_runtime_paths and runtime_paths:
+        print("\n\tRuntime search paths:")
+        for path in runtime_paths:
+            print(f"\t  {path}")
+
+    print()
+
+
 @app.command("show")
 def _show(
     wheel_or_so_file: Path = typer.Argument(
         ..., help="Path to wheel or a shared library file."
-    )
+    ),
+    show_runtime_paths: bool = typer.Option(
+        False,
+        "-r",
+        "--with-runtime-paths",
+        help="Show runtime paths.",
+    ),
 ):
     """
     Show shared library dependencies of a wheel or a shared library file.
     """
     try:
-        dependencies = show(wheel_or_so_file)
-        pprint(dependencies)
+        libraries = show(wheel_or_so_file)
+        for lib, (deps, runtime_paths) in libraries.items():
+            print_dylib(
+                lib, deps, runtime_paths, list(libraries.keys()), show_runtime_paths
+            )
     except Exception as e:
         raise e
 
@@ -41,6 +78,12 @@ def _repair(
         None,
         help="Directory to output repaired wheel or shared library. (default: overwrite the input file)",
     ),
+    show_runtime_paths: bool = typer.Option(
+        False,
+        "-r",
+        "--with-runtime-paths",
+        help="Show runtime paths.",
+    ),
 ):
     """
     Repair a wheel file: copy shared libraries to the wheel directory.
@@ -52,8 +95,11 @@ def _repair(
             output_dir,
             modify_rpath=True,
         )
-        dependencies = show(repaired_wheel)
-        pprint(dependencies)
+        libraries = show(repaired_wheel)
+        for lib, (deps, runtime_paths) in libraries.items():
+            print_dylib(
+                lib, deps, runtime_paths, list(libraries.keys()), show_runtime_paths
+            )
     except RuntimeError as e:
         raise e
 
--- auditwheel_emscripten/show.py
+++ auditwheel_emscripten/show.py
@@ -1,5 +1,6 @@
 import tempfile
 from pathlib import Path
+import os.path
 
 from .lib_utils import get_all_shared_libs_in_dir, sharedlib_regex
 from .module import parse_dylink_section
@@ -7,26 +8,31 @@ from .wheel_utils import is_emscripten_wheel, unpack
 from .wasm_utils import is_wasm_module
 
 
-def show_dylib(dylib_file: Path) -> list[str]:
+def show_dylib(dylib_file: Path) -> tuple[list[str], list[str]]:
     if not is_wasm_module(dylib_file):
         raise RuntimeError(f"{dylib_file} is not a WASM file")
 
     dylink = parse_dylink_section(dylib_file)
-    return dylink.needed
+    return dylink.needed, dylink.runtime_paths
 
 
-def show_wheel_unpacked(wheel_extract_dir: str | Path) -> dict[str, list[str]]:
+def show_wheel_unpacked(
+    wheel_extract_dir: str | Path,
+) -> dict[str, tuple[list[str], list[str]]]:
     dependencies = {}
 
     shared_libs = get_all_shared_libs_in_dir(wheel_extract_dir)
     for shared_lib in shared_libs:
-        deps = show_dylib(shared_lib)
-        dependencies[shared_lib.relative_to(wheel_extract_dir).as_posix()] = deps
+        deps, runtime_paths = show_dylib(shared_lib)
+        dependencies[shared_lib.relative_to(wheel_extract_dir).as_posix()] = (
+            deps,
+            runtime_paths,
+        )
 
     return dependencies
 
 
-def show_wheel(wheel_file: Path) -> dict[str, list[str]]:
+def show_wheel(wheel_file: Path) -> dict[str, tuple[list[str], list[str]]]:
     if not is_emscripten_wheel(wheel_file.name):
         raise RuntimeError(f"{wheel_file} is not an emscripten wheel")
 
@@ -37,7 +43,7 @@ def show_wheel(wheel_file: Path) -> dict[str, list[str]]:
         return show_wheel_unpacked(extract_dir)
 
 
-def show(wheel_or_so_file: str | Path) -> dict[str, list[str]]:
+def show(wheel_or_so_file: str | Path) -> dict[str, tuple[list[str], list[str]]]:
     file = Path(wheel_or_so_file)
     if not file.exists():
         raise RuntimeError(f"no such file: {file}")
@@ -53,3 +59,21 @@ def show(wheel_or_so_file: str | Path) -> dict[str, list[str]]:
         }
     else:
         raise RuntimeError(f"unknown file type: {file}")
+
+
+def locate_dependency(
+    base: str, dep: str, libraries: list[str], runtime_paths: list[str]
+) -> str | None:
+    """
+    Check if the dependencies exists in the wheel using runtime paths.
+    If the dependency exists, return the path to the dependency.
+    Otherwise, return None.
+    """
+    for path in runtime_paths:
+        relpath = path.replace("$ORIGIN", "..")
+        # Always use unix-style separators
+        candidate = os.path.normpath(str(Path(base) / relpath / dep)).replace("\\", "/")
+        if candidate in libraries:
+            return candidate
+
+    return None
